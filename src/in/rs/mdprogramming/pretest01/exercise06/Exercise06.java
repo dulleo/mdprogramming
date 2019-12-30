dc4198b68ecd4bd28102f700087b5618ccdb6016
Dusko Lucic <dulleo@gmail.com>
2019-12-30 11:52:48 -0700

    solution added

--- src/in/rs/mdprogramming/pretest01/exercise06/Exercise06.java
+++ src/in/rs/mdprogramming/pretest01/exercise06/Exercise06.java
@@ -1,5 +1,7 @@
 package in.rs.mdprogramming.pretest01.exercise06;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.util.SimpleTimeZone;
 
 /**
@@ -14,16 +16,24 @@ import java.util.SimpleTimeZone;
  */
 public class Exercise06 {
     public static void main(String[] args) {
-        int rows = 5;
-        for(int i=1;i<=rows;i++){
-            for(int j=1;j<rows*2;j++) {
-               if(j<=rows-i || j>=rows+i) {
-                   System.out.print("  ");
-               } else {
-                   System.out.print("# ");
-               }
+        try(Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter rows: ");
+            int rows = sc.nextInt();
+            for(int i=1;i<=rows;i++){
+                for(int j=1;j<rows*2;j++) {
+                    if(j<=rows-i || j>=rows+i) {
+                        System.out.print("  ");
+                    } else {
+                        System.out.print("# ");
+                    }
+                }
+                System.out.println();
             }
-            System.out.println();
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
